f953a6801fa91cd4410cb4bb99eca3017ad558ae
VirajParab <parabveer01@gmail.com>
2016-02-25 23:47:59 +0530

    Added the project into intellij

---
+++ src/Block.java
@@ -0,0 +1,16 @@
+/**
+ * Created by virajpa on 25/02/16.
+ */
+public class Block {
+    private String color;
+    private int x;
+    private int y;
+
+    void Block(String color,int x,int y){
+        this.color = color ;
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
